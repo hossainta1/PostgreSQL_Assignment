5f826e9e8501e24854db29ba43a9efc883dd733e
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-05-25 22:09:42 +0600

    Solve problem 1 & 2

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -58,4 +58,20 @@ INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VA
 (3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
 (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
-SELECT * FROM sightings;
+-- ////////....PostgreSQL Problems.........//
+
+-- 1️⃣ Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+
+INSERT INTO rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+
+
+
+-- 2️⃣ Count unique species ever sighted.
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings;
+
+
+
